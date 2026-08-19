4050bbd5626aa407c662b8153112053fbe8105bd
Devendra Kumar Sahu <devendrakumarrsahu05@gmail.com>
2026-08-20 00:21:36 +0530

    google provider added

---
+++ auth.js
@@ -0,0 +1,46 @@
+// auth.js
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import connectDb from "@/db/connectDb";
+import User from "@/models/User";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost:true,
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "github" || account?.provider === "google") {
+        if (!user.email) return false;
+
+        await connectDb();
+        const currentUser = await User.findOne({ email: user.email });
+        if (!currentUser) {
+          await User.create({
+            email: user.email,
+            username: user.email.split("@")[0],
+          });
+        }
+        return true;
+      }
+      return true;
+    },
+    async session({ session }) {
+      await connectDb();
+      const dbUser = await User.findOne({ email: session.user.email });
+      if (dbUser) {
+        session.user.name = dbUser.username;
+      }
+      return session;
+    },
+  },
+});
