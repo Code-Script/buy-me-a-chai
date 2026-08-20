19aac1bc8762d7100b91e207dcf6419d8434d121
Devendra Kumar Sahu <devendrakumarrsahu05@gmail.com>
2026-08-20 18:49:45 +0530

    env variable name changed

--- auth.js
+++ auth.js
@@ -6,15 +6,15 @@ import connectDb from "@/db/connectDb";
 import User from "@/models/User";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  trustHost:true,
+  trustHost: true,
   providers: [
     GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
   callbacks: {
